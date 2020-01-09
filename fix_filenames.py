044d2eeb70a0b4d581c1c5461fdd0e61a6c82c3a
Jon David Tannehill <jdtannehill2490@gmail.com>
2020-01-09 17:59:02 -0600

    Fixed & improved fix_filenames.py

--- fix_filenames.py
+++ fix_filenames.py
@@ -1,14 +1,38 @@
-import os
+import os, re, argparse
 
-while true:
-    directory = input('Enter the full, absolute path of the directory with files whose names you want to fix:\n')
-    if os.path.isdir(directory):
-        break
+
+def fix_filenames(directory, deep=False):
+    pattern = re.compile(r'[^a-zA-Z0-9_\.]')
+    if deep != False:
+        for root, subs, items in os.walk(directory):
+            for x in items:
+                if re.search(pattern, x):
+                    os.rename(x, re.sub(pattern, '_', x))
     else:
-        print(directory + 'is not a valid directory path, please try again.\n')
+        for x in os.listdir(directory):
+            if re.search(pattern, x):
+                os.rename(x, re.sub(pattern, '_', x))
 
-for x in os.listdir(directory):
-    if " " in x:
-        os.rename(x, x.replace(' ', '_'))
 
-print('Done!')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Enter absolute path for the directory whose filenames you want to fix.\n'
+        'use the -d or --deep option to fix filenames of all subdirectories too.'
+    )
+    parser.add_argument('directory', nargs='?', default=None)
+    parser.add_argument('-d', '--deep', nargs='?', type=bool, default=False)
+    args = parser.parse_args()
+    #
+    if args.directory == None:
+        directory = input(
+            'Enter the full, absolute path of the directory with files whose names you want to fix:\n'
+        )
+        if not os.path.isdir(directory):
+            raise TypeError(
+                directory + ' is not a valid (absolute) directory path!\n'
+                'Example: ' + os.getcwd()
+            )
+        if input('Enter "d" to fix the filenames of *ALL* subdirectories (think carefully!):') == 'd':
+            deep = True
+    #
+    fix_filenames(directory, deep)
