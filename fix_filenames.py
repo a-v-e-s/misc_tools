4b9c7bb140b5c6da18f800053eae3849d57c339d
Jon David Tannehill <jdtannehill2490@gmail.com>
2020-01-10 05:23:00 -0600

    fix a few more things with fix_filenames.py

--- fix_filenames.py
+++ fix_filenames.py
@@ -1,28 +1,49 @@
+"""
+fix_filenames.py
+
+A module to replace non-word, non-dot characters in filenames.
+Can optionally be applied to directory names as well.
+Offers a command-line interface and an importable function of the same name.
+
+Author: Jon David Tannehill
+"""
+
 import os, re, argparse
 
 
-def fix_filenames(directory, deep=False):
+def fix_filenames(directory, recursive=False, subdirs=False):
     pattern = re.compile(r'[^a-zA-Z0-9_\.]')
-    if deep != False:
+    if recursive == True:
         for root, subs, items in os.walk(directory):
             for x in items:
                 if re.search(pattern, x):
-                    os.rename(x, re.sub(pattern, '_', x))
+                    new_name = re.sub(pattern, '_', x)
+                    os.rename(os.path.join(root, x), os.path.join(root, new_name))
+            if subdirs == True:
+                for y in subs:
+                    if re.search(pattern, y):
+                        new_name = re.sub(pattern, '_', y)
+                        os.rename(os.path.join(root, y), os.path.join(root, new_name))
     else:
         for x in os.listdir(directory):
-            if re.search(pattern, x):
-                os.rename(x, re.sub(pattern, '_', x))
+            if subdirs == True or os.path.isfile(x):
+                if re.search(pattern, x):
+                    new_name = re.sub(pattern, '_', x)
+                    os.rename(os.path.join(directory, x), os.path.join(directory, new_name))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Enter absolute path for the directory whose filenames you want to fix.\n'
-        'use the -d or --deep option to fix filenames of all subdirectories too.'
+        'use the -d or --recursive option to fix filenames of all subdirectories too.'
     )
     parser.add_argument('directory', nargs='?', default=None)
-    parser.add_argument('-d', '--deep', nargs='?', type=bool, default=False)
+    parser.add_argument('-r', '--recursive', nargs='?', type=bool, default=False)
+    parser.add_argument('-d', '--subdirs', nargs='?', type=bool, default=False)
     args = parser.parse_args()
     #
+    recursive = False
+    subdirs = False
     if args.directory == None:
         directory = input(
             'Enter the full, absolute path of the directory with files whose names you want to fix:\n'
@@ -32,7 +53,14 @@ if __name__ == '__main__':
                 directory + ' is not a valid (absolute) directory path!\n'
                 'Example: ' + os.getcwd()
             )
-        if input('Enter "d" to fix the filenames of *ALL* subdirectories (think carefully!):') == 'd':
-            deep = True
+        if input('Enter "r" to fix filenames recursively in all subdirectories (be careful!):') == 'r':
+            recursive = True
+        if input('Enter "d" to fix the names of subdirectories (think carefully!):') == 'd':
+            subdirs = True
     #
-    fix_filenames(directory, deep)
+    else:
+        if args.recursive != False:
+            recursive = True
+        if args.subdirs != False:
+            subdirs = True
+    fix_filenames(directory, recursive, subdirs)
