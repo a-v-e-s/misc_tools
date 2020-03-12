c0f518ab358e4b37ac0a9da3f2d8e7d0d8d2b267
Jon David Tannehill <jdtannehill2490@gmail.com>
2020-03-12 09:02:14 -0500

    added weaver.py

---
+++ weaver.py
@@ -0,0 +1,38 @@
+from multiprocessing import Process, cpu_count
+from threading import Thread
+from time import sleep
+
+def weaver(targets, args, separate=True):
+    cpus = cpu_count()
+    cpus_to_use = cpus - 1 if cpus > 2 else 1
+    jobs = []
+    count = 0
+    for x in targets:
+        if separate:
+            w = Process(target=x, args=args[count])
+        else:
+            w = Thread(target=x, args=args[count])
+        jobs.append(w)
+        count += 1
+    #
+    running_jobs = []
+    for x in range(cpus_to_use):
+        try:
+            jobs[x].start()
+            running_jobs.append(jobs[x])
+            jobs.remove(jobs[x])
+        except IndexError:
+            break
+    #
+    while jobs:
+        sleep(0.5)
+        for x in running_jobs:
+            if not x.is_alive():
+                running_jobs.remove(x)
+                jobs[0].start()
+                running_jobs.append(jobs[0])
+                jobs.remove(jobs[0])
+                break
+    #
+    for x in running_jobs:
+        x.join()
