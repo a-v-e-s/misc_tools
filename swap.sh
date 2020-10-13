c8df3cebaa05561f9711054ce9b3670a0094e6a6
Jon David Tannehill <jdtannehill@tutanota.com>
2020-10-13 05:31:41 -0500

    added another function to swap.sh

--- swap.sh
+++ swap.sh
@@ -15,3 +15,13 @@ function swap {
         return 1
     fi
 }
+
+function date_difference {
+    echo "Enter Date 1 in the format yyyy-mm-dd :"
+    read date1
+    echo "Enter Date 2 in the format yyyy-mm-dd :"
+    read date2
+    difference=$(( `date -d "$date1" +%s` / 86400 - `date -d "$date2" +%s` / 86400 ))
+    echo $difference
+    return $difference
+}
