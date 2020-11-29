511e2e025581912558941e05b649ab040ae021c5
Jon David Tannehill <jdtannehill@tutanota.com>
2020-11-29 03:50:57 -0600

    added btos_cnat_raed_tihs.py from my little_tech repo

---
+++ btos_cnat_raed_tihs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+btos_cnat_raed_tihs.py:
+A big mddile fnegir to the aoimglrtihc cohnsiresp of txet
+"""
+
+import random, re, functools
+import tkinter as tk
+
+
+def parser(string):
+    # here is our regular expression:
+    patt = re.compile(r'\b[A-Za-z]{4,}\b')
+    # find the matches, build the new string
+    new_string = ''
+    for word in string.split():
+        if re.fullmatch(patt, word):
+            new_word = scrambler(word)
+            new_string += ''.join(new_word) + ' '
+        else:
+            new_string += word + ' '
+    # take off the last space and return it:
+    new_string.rstrip(' ')
+    return new_string
+
+
+def scrambler(word):
+    to_fill = [word[:1], word[-1:]]
+    to_b_scrambled = list(word[1:-1])
+    while len(to_b_scrambled):
+        choice = random.choice(to_b_scrambled)
+        tmp = to_fill[:-1]
+        tmp.append(choice)
+        tmp.append(str(to_fill[-1:][0]))
+        to_fill = tmp
+        to_b_scrambled.remove(choice)
+    new_word = ''.join(to_fill)
+    if new_word == word:
+        return scrambler(word)
+    else:
+        return new_word
+
+
+if __name__ == '__main__':
+    from sys import argv
+    output = parser(argv[1])
+    print(output)
